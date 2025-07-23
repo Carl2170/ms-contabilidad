3145bdab666a18408ffcc3f556a188a1a9a9eb0f
CVargas2170 <vargascarlos2170@gmail.com>
2025-07-22 23:44:13 -0400

    update cors v2

--- src/main/java/org/sistemaempresarial/mscontablidad/config/WebConfig.java
+++ src/main/java/org/sistemaempresarial/mscontablidad/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://conta-frontend-uddo.onrender.com")
+                        .allowedOrigins("http://localhost:4200", "https://conta-frontend-8jhc.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
